7abafb27671cf6f990fc9f733722cdc1c6b5280a
xioak <475914010@qq.com>
2017-07-20 00:41:08 +0800

    111

---
+++ news-manager-mvc/dubbo-provider/src/main/java/com/demo/dubbo/service/Provider.java
@@ -0,0 +1,23 @@
+package com.demo.dubbo.service;
+
+import com.alibaba.dubbo.container.Main;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by xioamac on 2017/6/10.
+ */
+public class Provider {
+
+    public static void main(String[] args) throws Exception {
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+//                new String[]{"dubbo-provider.xml"});
+//        context.start();
+//
+//        System.out.println("启动成功");
+//
+//        System.in.read(); // 按任意键退出
+
+        Main.main(args);
+    }
+
+}
